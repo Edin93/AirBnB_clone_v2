911d4ede38ec68465597a6986ea1d266ed01e576
edin93 <il_matador@hotmail.fr>
2020-04-12 21:25:50 +0100

    task 0 sed changed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,6 +15,6 @@ sudo echo -e "$content" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 old_config="server {"
-new_config="\\\n\tserver {\n\t\tlocation /hbnb_static/ {\n\t\t\talias /data/web_static/current;\n\t\t}\n\t}\n"
+new_config="\n\tserver {\n\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current;\n\t\t}\n\t}\n"
 sudo sed -i "s/^$old_config/$new_config/" /etc/nginx/sites-enabled/default
 sudo service nginx restart
