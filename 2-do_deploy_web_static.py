9f41986e14fa707f723381e12913bb8837357af6
edin93 <il_matador@hotmail.fr>
2020-04-13 01:45:55 +0100

    task 2 removed os path verification

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,6 @@
 Distributing an archive to your web servers, using the function do_deploy.
 """
 from fabric.api import local, lcd, put, env, run
-import os.path
 
 
 env.hosts = ['35.243.129.178', '3.91.29.66']
@@ -13,36 +12,33 @@ def do_deploy(archive_path):
     """
     Distributes an archive to your web servers.
     """
-    if not os.path.exists(archive_path):
+    fname = archive_path.split('/')[-1]
+    r = put(archive_path, "/tmp/")
+    if r.failed:
         return False
-    else:
-        fname = archive_path.split('/')[-1]
-        r = put(archive_path, "/tmp/")
-        if r.failed:
-            return False
-        path = '/data/web_static/releases/' + fname[:-4]
-        r = run('mkdir -p %s' % path)
-        if r.failed:
-            return False
-        r = run('tar -xzf /tmp/%s -C /data/web_static/releases/%s/' %
-                (fname, fname[:-4]))
-        if r.failed:
-            return False
-        r = run('rm /tmp/%s' % fname)
-        if r.failed:
-            return False
-        r = run('mv /data/web_static/releases/%s/web_static/*\
-        /data/web_static/releases/%s' % (fname[:-4], fname[:-4]))
-        if r.failed:
-            return False
-        r = run('rm -rf /data/web_static/releases/%s/web_static' % fname[:-4])
-        if r.failed:
-            return False
-        r = run('rm -rf /data/web_static/current')
-        if r.failed:
-            return False
-        r = run('ln -s /data/web_static/releases/%s /data/web_static/current'
-                % fname[:-4])
-        if r.failed:
-            return False
-        return True
+    path = '/data/web_static/releases/' + fname[:-4]
+    r = run('mkdir -p %s' % path)
+    if r.failed:
+        return False
+    r = run('tar -xzf /tmp/%s -C /data/web_static/releases/%s/' %
+            (fname, fname[:-4]))
+    if r.failed:
+        return False
+    r = run('rm /tmp/%s' % fname)
+    if r.failed:
+        return False
+    r = run('mv /data/web_static/releases/%s/web_static/*\
+    /data/web_static/releases/%s' % (fname[:-4], fname[:-4]))
+    if r.failed:
+        return False
+    r = run('rm -rf /data/web_static/releases/%s/web_static' % fname[:-4])
+    if r.failed:
+        return False
+    r = run('rm -rf /data/web_static/current')
+    if r.failed:
+        return False
+    r = run('ln -s /data/web_static/releases/%s /data/web_static/current'
+            % fname[:-4])
+    if r.failed:
+        return False
+    return True
