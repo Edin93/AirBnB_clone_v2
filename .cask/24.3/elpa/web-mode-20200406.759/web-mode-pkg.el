bfffe5a83553baa1d1d096fafc72ba4db4b9688a
edin93 <il_matador@hotmail.fr>
2020-04-22 18:42:19 +0100

    task 0 fix == error

--- .cask/24.3/elpa/web-mode-20200406.759/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20200406.759" "major mode for editing web templates" (quote ((emacs "23.1"))))
--- .cask/24.3/elpa/yasnippet-20200412.2350/yasnippet-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "yasnippet" "20200412.2350" "Yet another snippet extension for Emacs" (quote ((cl-lib "0.5"))))
