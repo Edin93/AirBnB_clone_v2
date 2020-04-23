3f3d8912b43d7c348ba5d333b3a1f4b70ab1553c
edin93 <il_matador@hotmail.fr>
2020-04-23 01:05:51 +0100

    reset state code

--- models/state.py
+++ models/state.py
@@ -14,23 +14,20 @@ class State(BaseModel, Base):
         name: input name
     """
     __tablename__ = 'states'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            "City",
-            cascade="all,delete",
-            backref="state"
-        )
-    else:
-        name = ''
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City",
+        cascade="all,delete",
+        backref="state"
+    )
 
-        @property
-        def cities(self):
-            """Returns the list of Cities with the correspondant state_id
+    @property
+    def cities(self):
+        """Returns the list of Cities with the correspondant state_id
         """
         cities = models.storage.all(City)
         cities_list = []
         for k, v in cities.items():
             if v.state_id == self.id:
                 cities_list.append(v)
-                return(cities_list)
+        return(cities_list)
